08b58eca50072a195531e58c9fbb9e2840bb3fed
John Doe <pitkwiecien@gmail.com>
2022-10-07 20:46:46 +0200

    Added @NoArgsConstructor to 2 Dtos to make the program work

--- src/main/java/com/pitkwiecien/SpringProject/models/ProductDto.java
+++ src/main/java/com/pitkwiecien/SpringProject/models/ProductDto.java
@@ -2,8 +2,10 @@ package com.pitkwiecien.SpringProject.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ProductDto {
     private int id;
