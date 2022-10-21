9e0c336be003574089d1cc280eae40333f697989
uczen <pitkwiecien@gmail.com>
2022-10-21 10:18:49 +0200

    Added many mapping methods to UserController and UserRepository

--- src/main/java/com/pitkwiecien/SpringProject/controllers/UserController.java
+++ src/main/java/com/pitkwiecien/SpringProject/controllers/UserController.java
@@ -3,8 +3,7 @@ package com.pitkwiecien.SpringProject.controllers;
 import com.pitkwiecien.SpringProject.models.UserDTO;
 import com.pitkwiecien.SpringProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,18 +12,33 @@ public class UserController {
     @Autowired
     public UserRepository userRepository;
 
-    @GetMapping("/")
-    public String main() {
-        return "GLOWNA";
-    }
-
-    @GetMapping("/api/liczba")
-    public String num() {
-        return "Strona Glowna";
-    }
-
-    @GetMapping("/api/users")
+    @GetMapping("/api/users/")
     public List<UserDTO> showUsers(){
         return userRepository.getUsers();
+    }
+
+    @PostMapping("/api/users/")
+    public int addUser(@RequestBody List<UserDTO> userDTOs){
+        return userRepository.addUsers(userDTOs);
+    }
+
+    @PutMapping("/api/users/updatePut/{id}")
+    public String putUser(){
+        return "TODO updatePut";
+    }
+
+    @PatchMapping("/api/users/updatePatch/{id}")
+    public String patchUser(){
+        return "TODO updatePatch";
+    }
+
+    @GetMapping("/api/users/{id}")
+    public UserDTO showUserById(@PathVariable("id") int id){
+        return userRepository.getUserById(id);
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    public int deleteUser(@PathVariable("id") int id){
+        return userRepository.deleteUser(id);
     }
 }
--- src/main/java/com/pitkwiecien/SpringProject/repositories/UserRepository.java
+++ src/main/java/com/pitkwiecien/SpringProject/repositories/UserRepository.java
@@ -16,4 +16,31 @@ public class UserRepository {
     public List<UserDTO> getUsers(){
         return jdbcTemplate.query("SELECT * FROM user", BeanPropertyRowMapper.newInstance(UserDTO.class));
     }
+
+    public UserDTO getUserById(int id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM user WHERE user.id=?",
+                BeanPropertyRowMapper.newInstance(UserDTO.class),
+                id
+        );
+    }
+
+    public int addUsers(List<UserDTO> userDTOs){
+        int newRows = 0;
+        for (UserDTO userDTO : userDTOs) {
+            newRows += jdbcTemplate.update(
+                    "INSERT INTO user(name, surname, email, role, password) VALUES(?,?,?,?,?)",
+                    userDTO.getName(),
+                    userDTO.getSurname(),
+                    userDTO.getEmail(),
+                    userDTO.getRole(),
+                    userDTO.getPassword()
+            );
+        }
+        return newRows;
+    }
+
+    public int deleteUser(int id) {
+        return jdbcTemplate.update("DELETE FROM user WHERE user.id=?;", id);
+    }
 }
